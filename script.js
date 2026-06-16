2b153722bb1068708a5cac66fa89fd347c635e5c
amos <amosmokua12@gmail.com>
2026-06-16 11:06:14 +0300

    add data types

---
+++ script.js
@@ -0,0 +1,41 @@
+// Primitive data types
+
+// Numbers
+let myNumber = 40;
+const pi = 3.142;
+
+// strings
+const greeting = "Hello, welcome to Javascript";
+
+console.log(greeting);
+console.log(pi);
+
+// Boolean
+let isFerrariAwesome = true;
+let isKimigreat = true;
+let above18 = false;
+
+console.log(isFerrariAwesome);
+
+// Null
+const name = null;
+
+
+// composite data types
+// Objects
+let student = {
+    name: "Maxson",
+    id: 3759427,
+    age: 21,
+    city: "Nairobi",
+    hasPaidFee: false
+};
+
+
+console.log(student);
+
+// Arrays
+let numbers = [34,67,45,97,977,433,67];
+
+let ft18 = ["maina", "Fiona", "Daniel", "Olentiki"];
+console.log(ft18);
